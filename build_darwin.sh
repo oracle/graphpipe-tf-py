a65fdc245aa7e1c40a9968cbe6c828249372e9f7
Nik Kriplani <nik.kriplani@oracle.com>
2018-10-16 16:06:59 -0700

    Added Copyright with upl reference

--- build_darwin.sh
+++ build_darwin.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#
+# Copyright (c) 2018, Oracle and/or its affiliates. All rights reserved.
+#
+# Licensed under the Universal Permissive License v 1.0 as shown at
+# http://oss.oracle.com/licenses/upl.
+
 set -ex
 
 brew install curl openssl
